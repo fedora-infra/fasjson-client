f7cb5619102d546b52aea2b405355866fab0f2df
Aurélien Bompard <aurelien@bompard.org>
2021-04-15 14:57:41 +0200

    Update dependencies
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/make-requirements.sh
+++ docs/make-requirements.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-here=`dirname $0`
+here=$(realpath $(dirname $0))
 output="${here}/requirements.txt"
 
 set -x
@@ -11,6 +11,3 @@ poetry export -f requirements.txt --without-hashes -o "${output}"
 
 # Remove the GSSAPI module because ReadTheDocs does not install C-based modules
 sed -i -e '/^gssapi==/d ; /^requests-gssapi==/d' "${output}"
-
-# Add toml to parse the version in conf.py
-echo toml >> "${output}"
