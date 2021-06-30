d54e116d56bb6fbb8630494534853daf5c1d5d52
Aurélien Bompard <aurelien@bompard.org>
2021-06-30 11:15:28 +0200

    Update readthedocs dependencies
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/make-requirements.sh
+++ docs/make-requirements.sh
@@ -9,5 +9,8 @@ set -x
 
 poetry export -f requirements.txt --without-hashes -o "${output}"
 
+# Remove the python versions annotations
+sed -i -e 's/; .\+$//' "${output}"
+
 # Remove the GSSAPI module because ReadTheDocs does not install C-based modules
 sed -i -e '/^gssapi==/d ; /^requests-gssapi==/d' "${output}"
