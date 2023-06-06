1d624af714f024f5717be3081637f1a1d50bf2c5
Aurélien Bompard <aurelien@bompard.org>
2023-06-06 16:31:01 +0200

    Replace dependabot with renovate
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -8,11 +8,7 @@ TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
 
 set -x
 
-# Note: we can't use poetry export because it isn't smart enough with conditional dependencies:
-# flake8 requires importlib_metadata on python < 3.8, so it's not installed, but it's exported
-# and liccheck crashes on packages listed in the req file but not installed.
-# poetry export --dev -f requirements.txt -o $TMPFILE
-
-poetry run pip freeze --exclude-editable --isolated > $TMPFILE
+poetry export --dev -f requirements.txt -o $TMPFILE
+# poetry run pip freeze --exclude-editable --isolated > $TMPFILE
 
 poetry run liccheck -r $TMPFILE
